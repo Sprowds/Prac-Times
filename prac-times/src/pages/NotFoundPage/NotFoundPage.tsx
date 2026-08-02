ec699a097aa34f0260b1440355e50bb418948a5b
Sprowds <saint_sprow@mail.ru>
2026-08-02 16:01:24 +0500

    prac-times 0.2.1
    - Not found page is ready

--- prac-times/src/pages/NotFoundPage/NotFoundPage.tsx
+++ prac-times/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,7 +1,23 @@
+import { NavLink } from "react-router";
 import styles from "./NotFoundPage.module.css";
 
 const NotFoundPage = () => {
-  return <div className={styles.not_found}>NotFoundPage</div>;
+  return (
+    <div className={styles.not_found}>
+      <div className="container">
+        <div className={styles.not_found__inner}>
+          <h1 className={styles.title}>404</h1>
+          <p className={styles.text}>
+            Самая большая ошибка в жизни — это брать одну бутылку вина и
+            считать, что хватит.
+          </p>
+          <NavLink to="/" className={styles.link}>
+            На главную
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NotFoundPage;
